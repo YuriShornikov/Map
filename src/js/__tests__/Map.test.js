9ced0494f9511ca7c136480a5a57c8408bcb83d0
Yuri Shornikov <dbonlyfun@gmail.com>
2024-02-09 21:19:34 +0300

    correct

--- src/js/__tests__/Map.test.js
+++ src/js/__tests__/Map.test.js
@@ -8,12 +8,11 @@ describe('ErrorRepository', () => {
 
         expect(errorRepository.translate(404)).toBe('Not Found');
         expect(errorRepository.translate(500)).toBe('Internal Server Error');
-    });
-
+    })
     test('Unknown error', () => {
         const errorRepository = new ErrorRepository();
         errorRepository.addError(404, 'Not Found');
 
         expect(errorRepository.translate(500)).toBe('Unknown error');
-    });
-});
+    })
+})
